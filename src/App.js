79e38881035d37c1f7f849ebcdba83bf55b1662c
ryeongeunkim <ryeongeun.gim@gmail.com>
2022-12-24 17:53:22 +0900

    경로설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import TodoListPage from './pages/TodoListPage';
+import TodoListItemPage from './pages/TodoListItemPage';
+import PostPage from './pages/PostPage';
 
 const App = () => {
-  return <div>App</div>
-}
+  return (
+    <>
+      <Routes>
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<SignUpPage />} path="/signup" />
+        <Route element={<TodoListPage />} path="/todolist" />
+        <Route element={<TodoListItemPage />} path="/todolistitem" />
+        <Route element={<PostPage />} path={'/'} exact />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
